acecc6d69172235613f40ce675a7cb5ceea9c595
Timelas <lom.a.s@icloud.com>
2023-06-22 21:14:19 +0300

    test

--- src/components/DressCode/Dresscode.js
+++ src/components/DressCode/Dresscode.js
@@ -6,7 +6,7 @@ function Dresscode() {
     <section className="dresscode" id="dresscode">
       <div className="dresscode__header">
         <h2 className="dresscode__title">Dress code</h2>
-        <div className="dresscode__trait">Black Tie</div>
+        <div className="dresscode__trait"></div>
       </div>
       <div className="dresscode__figure"></div>
       <p className="dresscode__description">Мы очень старались сделать праздник красивым и будем рады, если в своих нарядах вы поддержите цветовую гамму нашей свадьбы. Примеры образов ниже:</p>
